1d58f0e9cbf3f37284b9cb5e938e653252c3afbc
MiguelKersch <99051637@mydavinci.nl>
2020-05-19 12:16:02 +0200

    added functions to the pokemon class

--- class/Pokemon.php
+++ class/Pokemon.php
@@ -1,7 +1,8 @@
 <?php
 
 
-abstract class Pokemon{
+abstract class Pokemon
+{
 
     private $name;
     private $health;
@@ -14,34 +15,49 @@ abstract class Pokemon{
 
     static $population;
 
-	public function __construct($name , $maxhealth) {
-        $this -> name = $name;  
-        $this -> maxhealth = $maxhealth;
-        $this -> health = $maxhealth; 
-        
+    public function __construct($name, $maxhealth)
+    {
+        $this->name = $name;
+        $this->maxhealth = $maxhealth;
+        $this->health = $maxhealth;
+
         self::$population++;
-	}
-
-
+    }
 
     static function getPopulation()
     {
         return self::$population;
     }
-
+    
     public function getName()
     {
         return $this->name;
+    }
+
+    public function getMaxHealth()
+    {
+        return $this->maxhealth;
     }
 
     public function getHealth()
     {
         return $this->health;
     }
-    public function getMaxHealth()
+
+    public function getAttack()
     {
-        return $this->maxhealth;
+        return $this->attacks;
     }
-
+    public function getType()
+    {
+        return $this->type;
+    }
+    public function getResistance()
+    {
+        return $this->resistance;
+    }
+    public function getWeakness()
+    {
+        return $this->weakness;
+    }
 }
-
